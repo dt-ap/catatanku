3f7b110e99bdc4566350d9b63acba714a9b053ce
dt-ap <dt_ap@ymail.com>
2022-04-23 10:06:42 +0700

    initialize backend

---
+++ catatanku-backend/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	if os.Getenv("APP_ENV") != "production" {
+		godotenv.Load()
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
